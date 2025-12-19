cc638f4ee78b55d475a6b663b560a6634d1d7887
khaled93shadid <khaled_shadid@yahoo.com>
2025-12-20 00:36:09 +0300

    a

--- server/index.js
+++ server/index.js
@@ -15,7 +15,10 @@ const app =express()
 app.use(express.json())
 // app.use(cors());
 app.use(cors({
-  origin: ['http://localhost:5000', 'https://amazon-reactjs-lemon.vercel.app/'],
+  origin: ['http://localhost:5000',
+  'https://amazon-reactjs-kqw8tx0qc-khaled-shadids-projects.vercel.app',
+  'https://amazon-reactjs.vercel.app',
+  'https://*.vercel.app'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
