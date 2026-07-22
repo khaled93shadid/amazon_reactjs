582d1ec01d568b246983861dde192e4595c856b8
khaled93shadid <khaled_shadid@yahoo.com>
2026-07-22 19:28:00 +0300

    EDIT

--- server/config/db.js
+++ server/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose') ; require('dotenv').config();
 
 const connectdb = async ()=>{
     try{
-        await mongoose.connect(process.env.MONGO_URl)
+        await mongoose.connect(process.env.MONGO_URL)
     console.log("Mongoos DataBase Connected successfully 🛰️")
     console.log(" You Are In Good Hand 🔥 🎯")
     }//try
